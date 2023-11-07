d803afa3dad09ff40dfca3065d81b0851b76b98d
c1umbo4ka <banyalazr1127@gmail.com>
2023-11-07 20:28:39 +0300

    Работа над 1.1 завершена

---
+++ Task1_1/1_1/main.c
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+/**
+* @brief Функция расчёта по заданной формуле.
+* @param x Аргумент функции.
+* @param y Аргумент функции.
+* @param z Аргумент функции.
+* @return Возвращает значение функции.
+*/
+double getA(double x, double y, double z);
+
+
+/**
+* @brief Функция расчёта по заданной формуле.
+* @param x Аргумент функции.
+* @param y Аргумент функции.
+* @param z Аргумент функции.
+* @return Возвращает значение функции.
+*/
+double getB(double x, double y, double z);
+
+
+/**
+* @brief Точка входа в программу.
+* @return Возвращает 0 в случае успеха.
+*/
+int main()
+{
+	const double x = 0.2;
+	const double y = 0.004;
+	const double z = 1.1;
+	const double a = getA(x, y, z);
+	const double b = getB(x, y, z);
+	printf("x = %lf y = %lf z = %lf\n", x, y, z);
+	printf("a = %lf b = %lf", a, b);
+	return 0;
+}
+
+double getA(double x, double y, double z)
+{
+	return pow(sin(pow(x, 2) + z), 2) - sqrt(x / y);
+}
+
+double getB(double x, double y, double z)
+{
+	return (pow(x, 2) / z) + cos(pow((x + y), 3));
+}
