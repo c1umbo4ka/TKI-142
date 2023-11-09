3977e2c0a4172ec62ec6748c75ba5566c5f7fbb9
c1umbo4ka <banyalazr1127@gmail.com>
2023-11-09 09:39:41 +0300

    Работа с 2.2

---
+++ Task2_2/2_2/main.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+/**
+* @brief Функция расчёта по заданной формуле.
+* @param x Аргумент функции.
+* @param a Аргумент функции.
+* @return Возвращает значение функции.
+*/
+double getY(double x, double a);
+
+/**
+* @brief Точка входа в программу.
+* @return Возвращает 0 в случае успеха.
+*/
+int main()
+{
+	const double a = 20.3;
+	double x = 0;
+	printf("%s", "Insert x: ");
+	scanf_s("%lf", &x);
+	double y = getY(x, a);
+	printf("x = %lf\n", x);
+	printf("y = %lf", y);
+	return 0;
+}
+
+double getY(double x, double a)
+{
+	if (x > 1)
+	{
+		return log10(x + 1);
+	}
+	else
+	{
+		return sin(2 * sqrt(abs(a * x)));
+	}
+}
