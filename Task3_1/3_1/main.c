7c35abdbc3a65acd0375eecdc124850615b49639
c1umbo4ka <banyalazr1127@gmail.com>
2023-11-12 14:36:04 +0300

    Внесены изменения в 3.3

--- Task3_1/3_1/main.c
+++ Task3_1/3_1/main.c
@@ -6,9 +6,10 @@
 
 /**
 * @brief Функция расчета по заданной формуле.
+* @param x Аргумент функции.
 * @return Возвращает значение фенкции.
 */
-double gety();
+double gety(double x);
 
 /**
 * @brief Функция проверки переменной x.
@@ -23,8 +24,20 @@ bool checkx(double x);
 */
 int main()
 {
+	const double detx = 0.05;
 	double x;
-	gety();
+	for (x = 0; x <= 0.8 + detx; x += detx)
+	{
+		if (checkx(x))
+		{
+			gety(x);
+			printf("x = %lf y = %lf\n", x, gety(x));
+		}
+		else
+		{
+			printf("%s", "В данной точке функция не определена.");
+		}
+	}
 	return 0;
 }
 
@@ -40,19 +53,7 @@ bool checkx(double x)
 	}
 }
 
-double gety()
+double gety(double x)
 {
-	const double detx = 0.05;
-	for (double x = 0; x <= 0.8 + detx; x += detx)
-	{
-		if (checkx(x))
-		{
-			double y = tan(x) - (1 / 3) * pow(tan(x), 3) + (1 / 5) * pow(tan(x), 5) - (1 / 3);
-			printf("x = %lf y = %lf\n", x, y);
-		}
-		else
-		{
-			printf("%s", "В данной точке функция не определена.");
-		}
-	}
+	return tan(x) - (1 / 3) * pow(tan(x), 3) + (1 / 5) * pow(tan(x), 5) - (1 / 3);
 }
