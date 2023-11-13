cb5599cd110ce01d853492b693f88296b88e25f8
c1umbo4ka <banyalazr1127@gmail.com>
2023-11-13 15:08:16 +0300

    3.2

--- Task3_2/3_2/main.c
+++ Task3_2/3_2/main.c
@@ -1,36 +1,57 @@
 ﻿#include <stdio.h>
 #include <math.h>
 #include <float.h>
+#include <errno.h>
+#include <stdlib.h>
 
-double get_factorial(double variable);
+int get_factorial(int end);
 
-double get_sum(double end, double variable, double sum);
+double get_sum(int end);
 
 double get_value();
 
 int main()
 {
 	printf("%s", "Insert end of the segment: ");
-	double end = get_value();
-	printf("Summary = %lf", get_sum(end, variable, sum));
+	int end = get_value();
+	printf("Summary = %lf", get_sum(end));
 	return 0;
 }
 
-double get_factorial(double variable, double end)
+int get_factorial(int end)
 {
-	for (double variable = 1; variable < end + 1; variable++)
-	{
-		if (2 * variable == 0 || 2 * variable)
+	int factorial = 0;
+	for (int variable = 1; variable < end + 1; variable++)
+		if (variable * 2 == 0 || 2 * variable == 1)
 		{
-			return 1;
+			factorial = 1;
 		}
-	}
+		else
+		{
+			factorial = factorial * variable;
+		}
+	return factorial;
 }
-double get_sum(double end, double variable, double sum)
+
+double get_sum(int end)
 {
-	sum = 0;
-	for (double variable = 1; variable < end + 1; variable++)
+	double sum = 0;
+	for (int variable = 1; variable < end + 1; variable++)
 	{
-		double sum = sum 
+		sum = sum + (1.0 / (get_factorial(end)));
 	}
+	return sum;
+}
+
+double get_value()
+{
+	int value;
+	int result = scanf_s("%d", &value);
+	if (result != 1 || value <= 0)
+	{
+		errno = EIO;
+		perror("Invalid value entered!!!");
+		abort();
+	}
+	return value;
 }
