4aeb79b7e96e4b1a6dd855db31b47dcc85c61cbb
c1umbo4ka <banyalazr1127@gmail.com>
2023-11-13 11:26:08 +0300

    Начата работа над 3.2

---
+++ Task3_2/3_2/main.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <float.h>
+
+double get_factorial(double variable);
+
+double get_sum(double end, double variable, double sum);
+
+double get_value();
+
+int main()
+{
+	printf("%s", "Insert end of the segment: ");
+	double end = get_value();
+	printf("Summary = %lf", get_sum(end, variable, sum));
+	return 0;
+}
+
+double get_factorial(double variable, double end)
+{
+	for (double variable = 1; variable < end + 1; variable++)
+	{
+		if (2 * variable == 0 || 2 * variable)
+		{
+			return 1;
+		}
+	}
+}
+double get_sum(double end, double variable, double sum)
+{
+	sum = 0;
+	for (double variable = 1; variable < end + 1; variable++)
+	{
+		double sum = sum 
+	}
+}
