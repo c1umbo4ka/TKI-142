d3993faa03ff74e8a5f30f8f9404d6fefdd872c9
c1umbo4ka <banyalazr1127@gmail.com>
2023-11-15 20:53:48 +0300

    Внесены изменения в 2.1

--- Task2_1/2_1/main.c
+++ Task2_1/2_1/main.c
@@ -1,18 +1,26 @@
 ﻿#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /**
 * @brief Функция вывода дня недели и проверки ввода.
 * @param number Аргумент функции.
 * @return Возвращает название дня недели в случае успеха.
 */
-int getDay(int number);
+int getDay(enum day day);
 
 /**
 * @brief Функция вывода месяца и проверки ввода.
 * @param number Аргумент функции.
 * @return Возвращает название месяца в случае успеха.
 */
-int getMonth(int number);
+int getMonth(enum month month);
+
+enum choice
+{
+	Day = 1,
+	Month = 2
+};
 
 enum day
 {
@@ -25,7 +33,7 @@ enum day
 	Sunday
 };
 
-enum months 
+enum month
 {
 	January = 1,
 	February,
@@ -47,135 +55,186 @@ enum months
 */
 int main()
 {
-	int number;
-	printf("Enter a number to find the day of the week and month: ");
-	scanf_s("%d", &number);
-	getDay(number);
-	getMonth(number);
+	puts("Enter the number corresponding to the task:\n");
+	printf_s("%d - to display the day of the week\n", Day);
+	printf_s("%d - to display the month\n", Month);
+	int input = get_value();
+	enum UserInput user_choice = (enum choice)input;
+	switch (user_choice)
+	{
+		case Day:
+		{
+			printf_s("%s", "Enter a number to find the day of the week: ");
+			int number = get_value();
+			enum UserInput day = (enum choice)number;
+			getDay(day);
+			break;
+		}
+		case Month:
+		{
+			puts("Enter a number to find month: ");
+			int number = get_value();
+			enum UserInput month = (enum choice)number;
+			getMonth(month);
+			break;
+		}
+		default:
+		{
+			puts("Invalid choice.");
+			break;
+		}
+	}
 	return 0;
 }
 
-int getDay(int number)
+int getDay(enum day day)
 {
-	if (number >= 1 && number <= 7)
+	switch (day)
 	{
-		printf("%s", "Day of the week: ");
-		switch (number)
+		case Monday:
 		{
-			case Monday:
-			{
-				printf("%s\n", "Monday");
-				break;
-			}
-			case Tuesday:
-			{
-				printf("%s\n", "Tuesday");
-				break;
-			}
-			case Wednesday:
-			{
-				printf("%s\n", "Wensday");
-				break;
-			}
-			case Thursday:
-			{
-				printf("%s\n", "Thursday");
-				break;
-			}
-			case Friday:
-			{
-				printf("%s\n", "Friday");
-				break;
-			}
-			case Saturday:
-			{
-				printf("%s\n", "Saturday");
-				break;
-			}
-			case Sunday:
-			{
-				printf("%s\n", "Sunday");
-				break;
-			}
+			printf_s("%s", "Day of the week: ");
+			printf("%s\n", "Monday");
+			break;
+		}
+		case Tuesday:
+		{
+			printf_s("%s", "Day of the week: ");
+			printf("%s\n", "Tuesday");
+			break;
+		}
+		case Wednesday:
+		{
+			printf_s("%s", "Day of the week: ");
+			printf("%s\n", "Wensday");
+			break;
+		}
+		case Thursday:
+		{
+			printf_s("%s", "Day of the week: ");
+			printf("%s\n", "Thursday");
+			break;
+		}
+		case Friday:
+		{
+			printf_s("%s", "Day of the week: ");
+			printf("%s\n", "Friday");
+			break;
+		}
+		case Saturday:
+		{
+			printf_s("%s", "Day of the week: ");
+			printf("%s\n", "Saturday");
+			break;
+		}
+		case Sunday:
+		{
+			printf_s("%s", "Day of the week: ");
+			printf("%s\n", "Sunday");
+			break;
+		}
+		default:
+		{
+			printf_s("%s", "Invalid day of the week number.");
+			break;
 		}
 	}
-	else
-	{
-		printf("Invalid day of the week number.\n");
-	}
-	
 }
 
-int getMonth(int number)
+int getMonth(enum month month)
 {
-	if (number >= 1 && number <= 12)
+	
+	switch (month)
 	{
-		printf("%s", "Month: ");
-		switch (number)
+		case January:
 		{
-			case January:
-			{
-				printf("%s", "January");
-				break;
-			}
-			case February:
-			{
-				printf("%s", "February");
-				break;
-			}
-			case March:
-			{
-				printf("%s", "March");
-				break;
-			}
-			case April:
-			{
-				printf("%s", "April");
-				break;
-			}
-			case May:
-			{
-				printf("%s", "May");
-				break;
-			}
-			case June:
-			{
-				printf("%s", "June");
-				break;
-			}
-			case July:
-			{
-				printf("%s", "July");
-				break;
-			}
-			case August:
-			{
-				printf("%s", "August");
-				break;
-			}
-			case September:
-			{
-				printf("%s", "September");
-				break;
-			}
-			case October:
-			{
-				printf("%s", "October");
-				break;
-			}
-			case November:
-			{
-				printf("%s", "November");
-				break;
-			}
-			case December:
-			{
-				printf("%s", "December");
-				break;
-			}
+			printf_s("%s", "Month: ");
+			printf("%s\n", "January");
+			break;
+		}
+		case February:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "February");
+			break;
+		}
+		case March:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "March");
+			break;
+		}
+		case April:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "April");
+			break;
+		}
+		case May:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "May");
+			break;
+		}
+		case June:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "June");
+			break;
+		}
+		case July:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "July");
+			break;
+		}
+		case August:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "August");
+			break;
+		}
+		case September:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "September");
+			break;
+		}
+		case October:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "October");
+			break;
+		}
+		case November:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "November");
+			break;
+		}
+		case December:
+		{
+			printf_s("%s", "Month: ");
+			printf("%s\n", "December");
+			break;
+		}
+		default:
+		{
+			printf_s("%s", "Invalid mounth number.");
+			break;
 		}
 	}
-	else {
-		printf("Invalid month number.\n");
+}
+
+int get_value()
+{
+	int value;
+	int result = scanf_s("%d", &value);
+	if (result != 1)
+	{
+		errno = EIO;
+		perror("Invalid value entered!!!");
+		abort();
 	}
+	return value;
 }
