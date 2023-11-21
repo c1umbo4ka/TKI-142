005ab461bc88681b24817833216cd9520eb4f79d
c1umbo4ka <banyalazr1127@gmail.com>
2023-11-21 20:35:45 +0300

    Внесены изменения в 4.1

--- Task4_1/4_1/main.c
+++ Task4_1/4_1/main.c
@@ -48,14 +48,14 @@ void true_array(int* my_array);
 * @param size - длинна массива.
 * @param my_array - массив.
 */
-void get_manual_input_array(int* my_array, const size_t size);
+void get_manual_input_array(int* my_array, const size_t size, const int minimum_limit, const int maximum_limit);
 
 /**
 * @brief Функция для заполнения массива случайными числами.
 * @param size - длинна массива.
 * @param my_array - массив.
 */
-void get_random_input_array(int* my_array, const size_t size);
+void get_random_input_array(int* my_array, const size_t size, const int minimum_limit, const int maximum_limit);
 
 /**
 * @brief Функция для печати массива.
@@ -76,9 +76,8 @@ int get_minimum_element(int* my_array, const size_t size);
 * @brief Функция вывода индексов элементов, значение которых больше предыдущего (начиная со второго).
 * @param my_array - массив.
 * @param size - длинна массива.
-* @return Индекс элементов.
 */
-size_t get_number_of_elements_that_more_than_previous(int* my_array, const size_t size);
+void print_number_of_elements_that_more_than_previous(int* my_array, const size_t size);
 
 /**
 * @brief Функция для нахождения количества пар соседних элементов с разными знаками.
@@ -100,6 +99,9 @@ int main()
 	true_size(int_size);
 	size_t size = (size_t)(int_size);
 
+	const int minimum_limit = get_value("Введите минимум массива: ");
+	const int maximum_limit = get_value("Введите максимум массива: ");
+
 	int* my_array = (int*)malloc(size * sizeof(int));
 	true_array(my_array);
 
@@ -115,12 +117,12 @@ int main()
 	{
 	case MANUAL:
 	{
-		get_manual_input_array(my_array, size);
+		get_manual_input_array(my_array, size, minimum_limit, maximum_limit);
 		break;
 	}
 	case RANDOM:
 	{
-		get_random_input_array(my_array, size);
+		get_random_input_array(my_array, size, minimum_limit, maximum_limit);
 		break;
 	}
 	default:
@@ -137,9 +139,11 @@ int main()
 
 
 	printf_s("Ответ на второе задание:\n");
-	get_number_of_elements_that_more_than_previous(my_array, size);
+	print_number_of_elements_that_more_than_previous(my_array, size);
 
 	printf_s("Ответ на третье задание: %s", get_two_pair_with_same_sign(my_array,size));
+	free(my_array);
+	return 0;
 }
 
 int get_value(const char* message)
@@ -176,13 +180,12 @@ void true_array(int* my_array)
 	}
 }
 
-void get_manual_input_array(int* my_array, const size_t size)
+void get_manual_input_array(int* my_array, const size_t size, const int minimum_limit, const int maximum_limit)
 {
 	for (size_t i = 0; i < size; i++)
 	{
-		const int minimum_limit = -20;
-		const int maximum_limit = 40;
-		int c = get_value("Введите число от -20 до 40: ");
+		printf("Введите число от %d до %d: ", minimum_limit, maximum_limit);
+		int c = get_value("");
 		if (c < minimum_limit || c > maximum_limit)
 		{
 			errno = EIO;
@@ -193,13 +196,13 @@ void get_manual_input_array(int* my_array, const size_t size)
 	}
 }
 
-void get_random_input_array(int* my_array, const size_t size)
+void get_random_input_array(int* my_array, const size_t size, const int minimum_limit, const int maximum_limit)
 {
 	unsigned int ttime = time(NULL);
 	srand(ttime);
 	for (size_t i = 0; i < size; i++)
 	{
-		my_array[i] = -20 + rand() % 61;
+		my_array[i] = minimum_limit + rand() % (maximum_limit - minimum_limit + 1);
 	}
 }
 
@@ -216,7 +219,7 @@ int get_minimum_element(int* my_array, const size_t size)
 	return minimum_element;
 }
 
-size_t get_number_of_elements_that_more_than_previous(int* my_array, const size_t size)
+void print_number_of_elements_that_more_than_previous(int* my_array, const size_t size)
 {
 	for (size_t i = 1; i < size; i++)
 	{
@@ -232,7 +235,7 @@ size_t get_two_pair_with_same_sign(int* my_array, const size_t size)
 	int quantity = 0;
 	for (size_t i = 1; i < size; i++)
 	{
-		if ((my_array[i] > 0 && my_array[i - 1] > 0) || (my_array[i] < 0 && my_array[i - 1] < 0))
+		if ((my_array[i] * my_array[i - 1] > 0))
 		{
 			quantity += 1;
 		}
